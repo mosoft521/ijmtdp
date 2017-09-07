618ae8725d54ae38cd7ba843a01ff2228cbbbd52
zhangjiawen <zhangjiawen@vcredit.com>
2017-09-07 12:47:14 +0800

    add chapter introduction1 2

---
+++ src/main/java/introduction1/qi1_3/Main.java
@@ -0,0 +1,11 @@
+package introduction1.qi1_3;
+
+public class Main {
+    public static void main(String[] args) {
+        new PrintThread("*").run();
+        new PrintThread("+").run();
+    }
+}
+/*
+
+ */
---
+++ src/main/java/introduction1/singleThread/Main.java
@@ -0,0 +1,9 @@
+package introduction1.singleThread;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            System.out.print("Good!");
+        }
+    }
+}
