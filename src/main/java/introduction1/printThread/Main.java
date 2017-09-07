7f3e1ad86429ac57b5669c2e524180ed0fd30893
zhangjiawen <zhangjiawen@vcredit.com>
2017-09-07 12:46:55 +0800

    add chapter introduction1

---
+++ src/main/java/introduction1/printThread/Main.java
@@ -0,0 +1,11 @@
+package introduction1.printThread;
+
+public class Main {
+    public static void main(String[] args) {
+        new PrintThread("Good!").start();
+        new PrintThread("Nice!").start();
+    }
+}
+/*
+
+ */
---
+++ src/main/java/introduction1/printer/Main.java
@@ -0,0 +1,11 @@
+package introduction1.printer;
+
+public class Main {
+    public static void main(String[] args) {
+        new Thread(new Printer("Good!")).start();
+        new Thread(new Printer("Nice!")).start();
+    }
+}
+/*
+
+ */
